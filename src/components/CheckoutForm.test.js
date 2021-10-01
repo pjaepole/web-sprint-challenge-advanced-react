27cfa93c1f222817a64a00cce9d53fcc287e2998
Jaehun Park <pjaehun4122@gmail.com>
2021-10-01 11:11:16 -0700

    finish setting up test for checkout form

--- src/components/CheckoutForm.test.js
+++ src/components/CheckoutForm.test.js
@@ -1,10 +1,42 @@
 import React from "react";
 import MutationObserver from 'mutationobserver-shim';
-import { render } from "@testing-library/react";
+import { render,screen } from "@testing-library/react";
 import CheckoutForm from "./CheckoutForm";
+import userEvent from '@testing-library/user-event'
 
 // Write up the two tests here and make sure they are testing what the title shows
+test("renders without errors", () => {
+    render(<CheckoutForm/>);
+    const header =screen.getByText(/checkout Form/i);
+    expect(header).toBeInTheDocument();
+});
 
-test("renders without errors", () => {});
+test("shows success message on submit with form details", () => {
+    render(<CheckoutForm></CheckoutForm>);
+    const firstName=screen.getByLabelText(/First Name:/i)
+    const lastName=screen.getByLabelText(/Last Name:/i)
+    const address=screen.getByLabelText(/Address:/i)
+    const city=screen.getByLabelText(/City:/i)
+    const state=screen.getByLabelText(/State:/i)
+    const zip=screen.getByLabelText(/Zip:/i)
+    const submitButton=document.querySelector('button')
 
-test("shows success message on submit with form details", () => {});
+    userEvent.type(firstName,'jae')
+    userEvent.type(lastName,'park')
+    userEvent.type(address,'kameHouse')
+    userEvent.type(city,'z city')
+    userEvent.type(state,'idk')
+    userEvent.type(zip,'12345')
+    userEvent.click(submitButton)
+
+    const successMsg=document.querySelector('[data-testid="successMessage"]')
+
+    // expect(successMsg).toHaveTextContent(/simpson/)//just testing it to break to see if its working
+    expect(successMsg).toHaveTextContent(/jae/)
+    expect(successMsg).toHaveTextContent(/park/)
+    expect(successMsg).toHaveTextContent(/z city/)
+    expect(successMsg).toHaveTextContent(/idk/)
+    expect(successMsg).toHaveTextContent(/12345/)
+    expect(successMsg).toHaveTextContent(/kameHouse/)
+
+});
